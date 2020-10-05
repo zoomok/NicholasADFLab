a49825477d458a7f3761f3809dcb10e2fcc0108d
zoomok <40251779+zoomok@users.noreply.github.com>
2020-10-05 18:25:29 +1100

    Create 003_PL_DATA_ASQL_Incr_Load.sql

---
+++ Tutorials/003_PL_DATA_ASQL_Incr_Load.sql
@@ -0,0 +1,6 @@
+--===========================================================================================================================
+-- Lab 3 : Incremental Data Load
+--===========================================================================================================================
+Full or Incremental data load from 0318 (Source) to 0319 (Staging)
+
+Link : https://www.udemy.com/course/implementing-real-world-use-cases-in-azure-data-factory-v2/learn/lecture/19940682#overview
