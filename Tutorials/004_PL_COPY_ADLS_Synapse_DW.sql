94801c46837898e03ff61e68c1ea5386ed09ca29
zoomok <40251779+zoomok@users.noreply.github.com>
2020-10-05 18:26:05 +1100

    Create 004_PL_COPY_ADLS_Synapse_DW.sql

---
+++ Tutorials/004_PL_COPY_ADLS_Synapse_DW.sql
@@ -0,0 +1,3 @@
+Load FactTransactionHistory in datalake to Azure DW using Azure Data Factory
+
+Link : https://www.udemy.com/course/azure-sql-data-warehouse-synapse-analytics-service/learn/lecture/18542416#overview
