ef4e997bd1248bc239d1f31ec61f194c0a994052
zoomok <40251779+zoomok@users.noreply.github.com>
2020-10-05 20:56:33 +1100

    Update 005_PL_DATA_SCD_Type1.sql

--- Tutorials/005_PL_DATA_SCD_Type1.sql
+++ Tutorials/005_PL_DATA_SCD_Type1.sql
@@ -8,3 +8,13 @@ https://www.udemy.com/course/implementing-real-world-use-cases-in-azure-data-fac
 2. SQL : Slow_Changing_Dimension_Type1.sql
 
 3. Pipeline Name : 005_PL_DATA_SCD_Type1
+
+4. Supported source connectors in mapping Data flow
+    - Azure Blog Storage
+    - Azure Data lake storage Gen1
+    - Azure Data lake storage Gen2
+    - Azure Synapse Analytics
+    - Azure SQL Database
+    - Azure CosmosDB
+    
+5. 
