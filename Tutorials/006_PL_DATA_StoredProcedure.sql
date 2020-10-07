385aa53b7d5f07edad888e4c9f1dece909e51a8d
zoomok <40251779+zoomok@users.noreply.github.com>
2020-10-07 11:02:53 +1100

    Create 006_PL_DATA_StoredProcedure.sql

---
+++ Tutorials/006_PL_DATA_StoredProcedure.sql
@@ -0,0 +1,10 @@
+--======================================================================================================================================
+-- Lab 6 : Azure Data Factory Stored Procedure Activity Transformation Activities
+--======================================================================================================================================
+1. Scenario   :
+    - Stored Procedure Activity could be used to run regular batch processes, to log pipeline execution progress or exceptions.
+    - We will create a simple stored procedure in the SQL0318 database to store pipeline name, pipeline run ID and sample text.
+
+2. Pipeline name : 006_PL_DATA_StoredProcedure
+
+3. 
