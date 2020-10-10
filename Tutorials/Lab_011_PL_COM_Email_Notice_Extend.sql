249aa19aa9a151a95d21292e9f474ff890d75a62
zoomok <40251779+zoomok@users.noreply.github.com>
2020-10-10 17:21:41 +1100

    Update and rename 011_PL_COM_Email_Notice_Extend.sql to Lab_011_PL_COM_Email_Notice_Extend.sql

--- Tutorials/Lab_011_PL_COM_Email_Notice_Extend.sql
+++ Tutorials/Lab_011_PL_COM_Email_Notice_Extend.sql
@@ -16,3 +16,5 @@
 				ErrorMessage : @{activity('Stored procedure for logging').Error.Message}
 
 2. Run pipeline and check email
+
+--(End)----------------------------------------------------------------------------------------------------------------------
