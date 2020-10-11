68470674e9c48886584c0b8dff59e6dcdfa8128b
zoomok <40251779+zoomok@users.noreply.github.com>
2020-10-11 18:36:03 +1100

    Create Lab_012_ADF_CI_CD.sql

---
+++ Labs/Lab_012_ADF_CI_CD.sql
@@ -0,0 +1,15 @@
+--===========================================================================================================================
+-- Lab 12 : Continuous integration and delivery (CI/CD) in ADF
+--===========================================================================================================================
+1. Link : https://www.youtube.com/watch?v=jJcikWOUqOk&list=PLMWaZteqtEaLTJffbbBzVOv9C0otal1FO&index=76
+
+	* CI : Continuous Integration
+	* CD : Continuous Delivery
+
+- Two methods :
+1. Automated deployment using Data Factory`s integration with Azure Pipelines (O)
+2. Manually upload a Resource Manager template using Data factory UX integration with Azure Resouce Manager (X)
+
+-- Very long tutorial, please refer to link
+
+--(End)----------------------------------------------------------------------------------------------------------------------
